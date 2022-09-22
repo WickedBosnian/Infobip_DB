2496b3d0b77abdb59eebe988a32a6512ef3164f5
Nino Omić <wicked.bosnian@hotmail.com>
2022-09-22 02:50:54 +0200

    -Dodana nova funkcija za vracanja broja klijenata -Prilagodjena procedura GetAllClients za paginaciju

---
+++ CarRental_DB/CarRental_DB/dbo/Functions/CountOfClients.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		Nino Omic
+-- Create date: 21.09.2022
+-- Description:	Returns count of clients
+-- =============================================
+CREATE FUNCTION CountOfClients()
+RETURNS int
+AS
+BEGIN
+	DECLARE @CountOfClients INT
+	SET @CountOfClients = (SELECT COUNT(ClientID) FROM Client)
+	RETURN @CountOfClients
+END
--- CarRental_DB/CarRental_DB/dbo/Stored Procedures/GetAllClients.sql
+++ CarRental_DB/CarRental_DB/dbo/Stored Procedures/GetAllClients.sql
@@ -1,4 +1,13 @@
-﻿CREATE PROCEDURE [dbo].[GetAllClients]
+﻿-- =============================================
+-- Author:		Nino Omic
+-- Create date: 19.09.2022
+-- Description:	Returns all records from Client table
+-- =============================================
+CREATE PROCEDURE [dbo].[GetAllClients]
+	@PageNumber INT = 1, @RowsPerPage INT = 10
 AS
 SELECT *
 FROM Client
+ORDER BY ClientID
+OFFSET ((@PageNumber - 1) * @RowsPerPage) ROWS
+FETCH NEXT @RowsPerPage ROWS ONLY;
